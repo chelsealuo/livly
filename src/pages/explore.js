3a73d081797f9b8f2432515b3ef75a77f8d9128a
Chelsea <cl773@cornell.edu>
2021-05-05 05:05:34 -0400

    add

--- src/pages/explore.js
+++ src/pages/explore.js
@@ -1,5 +1,6 @@
+/* global google */
 import React from 'react'
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
 
 const containerStyle = {
   paddingTop:'60px',
@@ -12,43 +13,37 @@ const center = {
   lng: -76.501884
 };
 
-const myLatLng = { lat: 42.443962, lng: -76.501884 };
-
-
 function Explore() {
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: "AIzaSyA7umAlHeG5FwqqxC0emqR7MTyugFN755Q"
   })
 
-  const [map, setMap] = React.useState(null)
+  // const [map, setMap] = React.useState(null)
+  //
+  // const onLoad = React.useCallback(function callback(map) {
+  //   const bounds = new window.google.maps.LatLngBounds();
+  //   map.fitBounds(bounds);
+  //   setMap(map)
+  // }, [])
+  //
+  // const onUnmount = React.useCallback(function callback(map) {
+  //   setMap(null)
+  // }, [])
 
-  const onLoad = React.useCallback(function callback(map) {
-    const bounds = new window.google.maps.LatLngBounds();
-    map.fitBounds(bounds);
-    setMap(map)
-  }, [])
-
-  const onUnmount = React.useCallback(function callback(map) {
-    setMap(null)
-  }, [])
-
-  new window.google.maps.Marker({
-    position: myLatLng,
-    map,
-    title: "Hello World!",
-  });
 
   return isLoaded ? (
       <GoogleMap
         mapContainerStyle={containerStyle}
+        initialCenter={center}
         center={center}
-        zoom={14}
-        onLoad={onLoad}
-        onUnmount={onUnmount}
+        zoom={15}
+        // onLoad={onLoad}
+        // onUnmount={onUnmount}
       >
-        { /* Child components, such as markers, info windows, etc. */ }
+        {<Marker position={{ lat: 42.443962, lng: -76.502884 }} />}
         <></>
+
       </GoogleMap>
   ) : <></>
 }
